800168ff034da62a2cecfbbb0e6a30cc754ad96b
HallanCosta <hallan.costa1@hotmail.com>
2025-10-19 02:15:50 -0300

    fix: ecosystem.config.cjs of pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,12 +2,15 @@ module.exports = {
   apps: [
     {
       name: "kyc",
-      script: "pnpm",
-      args: "start",
       cwd: "./",
+      script: "pnpm",
+      args: "preview --host 0.0.0.0 --port 4173 --strictPort",
+      env: { NODE_ENV: "production" },
       autorestart: true,
       watch: false,
       max_memory_restart: "500M",
     },
   ],
-};
+}
+
+
